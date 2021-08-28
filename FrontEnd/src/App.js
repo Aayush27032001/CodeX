924431aa6c57a26181b69634091781c3fbc5b195
Aayush Vishwakarma <aayushvishwakarma1999@gmail.com>
2021-08-28 20:25:47 +0530

    Second Commit

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,22 @@
 import './CSS/Navbar.css';
 import Navbar from './Components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Course from './Components/Course';
 
 function App() {
   return (
 
     <Router>
         <Navbar/> 
+        
+        <Switch>
+          <Route path='/about'>
+            <p>Hello</p>
+          </Route>
+          <Route path='/courses'>
+            <Course/>
+          </Route>
+        </Switch>
     </Router>
 
   );
