e02c83c6a65f2b9110c0731bf37280a5cf162120
Aayush Vishwakarma <aayushvishwakarma1999@gmail.com>
2021-08-30 21:41:42 +0530

    Third Commit

--- FrontEnd/src/Components/Navbar.jsx
+++ FrontEnd/src/Components/Navbar.jsx
@@ -8,28 +8,21 @@ export default class Navbar extends Component {
     render() {
 
         return (
-            <div>
-                <nav className="nav-container">
-                    <Link to='/about'><div className="nav-brand">
-                        <img src="Logo.png" alt="CodeX" width="130px" />
-                    </div></Link>
+            <div className="navbar">
+                <Link><div className="nav-brand">
+                    <img src="Logo.png" alt="CodeX" width="130px" />
+                </div></Link>
 
-                    <label for="check" class="checkbtn burger">
-                        <button className="checkbox" ><i class="fas fa-bars fa-2x "></i> </button>  
-                    </label>
-                    
-                    <ul className="nav-items-1">
-                        <li><Link to='/login'><span className="nav-box-item">Login</span></Link></li>
-                        <li><Link to='/Contributor'><span className="nav-box-item">Be A Contributor</span></Link></li>
-                        <li><span className='nav-items'><Link to='/Test'>Interview Experience</Link></span></li>
-                        <li><span className='nav-items'><Link to='/Test'>Test</Link></span></li>
-                        <li><span className='nav-items'><Link to='/Test'>Tutorials</Link></span></li>
-                        <li><span className='nav-items'><Link to='/Test'>Blog</Link></span></li>
-                        <li><span className='nav-items'><Link to='/courses'>Course</Link></span></li>
-                        <li><span className='nav-items'><Link to='/'>Home</Link></span></li>
-                    </ul>
-                </nav> 
-                
+                <ul className="nav-items-1">
+                    <li><span className='nav-items'><Link to='/'>Home</Link></span></li>
+                    <li><span className='nav-items'><Link to='/courses'>Course</Link></span></li>
+                    <li><span className='nav-items'><Link to='/Test'>Blog</Link></span></li>
+                    <li><span className='nav-items'><Link to='/Test'>Tutorials</Link></span></li>
+                    <li><span className='nav-items'><Link to='/Test'>Test</Link></span></li>
+                    <li><span className='nav-items'><Link to='/Test'>Interview Experience</Link></span></li>
+                    <li><Link to='/Contributor'><span className="nav-box-item">Be a Contributor</span></Link></li>
+                    <li><Link to='/login'><span className="nav-box-item">Login</span></Link></li>
+                </ul>
             </div>
         )
     }
