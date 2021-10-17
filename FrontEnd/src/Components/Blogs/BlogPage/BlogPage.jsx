014e36e907d045f1ab26156c9779e670f6db0804
Aayush Vishwakarma <aayushvishwakarma1999@gmail.com>
2021-10-17 15:20:29 +0530

    Added Loader to Blog

--- FrontEnd/src/Components/Blogs/BlogPage/BlogPage.jsx
+++ FrontEnd/src/Components/Blogs/BlogPage/BlogPage.jsx
@@ -2,35 +2,42 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Cards from '../BlogCard/BlogCard'
 import './BlogPage.css'
-// import blogimg from "../assets/1\ \(1\).png"
 import blogimg from '../../../assets/1\ \(1\).png'
-function BlogPage({ blogs }) {
+import loadingPNG from '../../../assets/loading.png'
+
+function BlogPage({ blogs, loading }) {
 
     return (
-        <div className="blog-page-container">
-            <div className="blog-flex">                
-                <div className="create-blog">
-                    <p className='blog-flex-text'>It is Great to Express your<br/> Ideas</p>
-                    <Link className="create-blog-link" to="/createBlog">Write your blog</Link>
+        <>
+            
+            {loading ? <div class="loader">
+                        <img className='loading-img' src={loadingPNG} width="4%" alt="Loading" />
+                        <h3>Loading...</h3>
+                    </div>
+                :
+                <div className="blog-page-container">
+                    <div className="blog-flex">
+                        <div className="create-blog">
+                            <p className='blog-flex-text'>It is Great to Express your<br /> Ideas</p>
+                            <Link className="create-blog-link" to="/createBlog">Write your blog</Link>
+                        </div>
+                        <div className="blog-flex-image">
+                            <img src={blogimg} width="70%" alt="" />
+                        </div>
+                    </div>
+
+                    <div className="blog-container">
+                        {
+                            blogs.map((blog) => {
+
+                                console.log(blog)
+                                return <Cards key={blog._id} blog={blog} />
+                            })
+                        }
+                    </div>
                 </div>
-                <div className="blog-flex-image">
-                    <img src={blogimg} width="70%" alt="" />
-                </div>
-            </div>
-
-            <div className="blog-container">
-
-
-                {
-                    blogs.map((blog) => {
-
-                        console.log(blog)
-                        return <Cards key={blog._id} blog={blog} />
-                    })
-                }
-            </div>
-        </div>
-
+            }
+        </>
     )
 }
 
