b789cb05723500ed702b277ecd2222cab0c43b1c
Aayush Vishwakarma <aayushvishwakarma1999@gmail.com>
2021-10-19 20:13:03 +0530

    Edited Blog Page

--- FrontEnd/src/Components/Test/CreateTestForm/CreateTestForm.jsx
+++ FrontEnd/src/Components/Test/CreateTestForm/CreateTestForm.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 
-function CreateTestForm() {
+function CreateTestForm({add}) {
+    var question = []
+    
+    function addQuestion() {
+        var title = document.getElementById("question").value;
+        var option1 = document.getElementById("1").value;
+        var option2 = document.getElementById("2").value;
+        var option3 = document.getElementById("3").value;
+        var option4 = document.getElementById("4").value;
+        console.log(title);
+        question.push(
+            question = {title,option1,option2,option3,option4}
+
+        )
+        console.log(question);
+        add(question);
+    }
 
     return (
         <div className='createtest-form-container'>
-            <form className='createtest-form' action="POST">
+            <div className='createtest-form'>
                 <input type="text" name="question" id="question" className='test-question input-field' placeholder='Enter Your Question' />
                 <input type="text" name="question-option" id="1" className='test-option input-field' placeholder='Option' />
                 <input type="text" name="question-option" id="2" className='test-option input-field' placeholder='Option' />
                 <input type="text" name="question-option" id="3" className='test-option input-field' placeholder='Option' />
                 <input type="text" name="question-option" id="4" className='test-option input-field' placeholder='Option' />     
-                <input type="submit" value="Submit"/>
-            </form>
+                <input onClick={()=>addQuestion()} value="Submit"/>
+            </div>
         </div>
     )
 }
